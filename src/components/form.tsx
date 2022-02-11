af785bea0ad2d781ac1aec3e7485a3410aed5b69
Roohollah Khodabakhshi <roohollah.khodabakhshi@specific-group.com>
2022-02-11 10:04:15 +0330

    use hook and result panel in form

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -2,17 +2,32 @@ import SimpleCreator from "../utils/classes/simpleCreator";
 import shapeNames from "../utils/common";
 import ShapeForm from "./shapeForm";
 import { ShapeDimensionParam } from "../utils/classes/shapeDimensionParam";
-import React from 'react';
+import React, { useState } from 'react';
 
 function Form() {
+   let result: string[] = [];
+
+   const [resultItems, setResultItems] = useState({
+      jsx: result.map((item, index) =>
+         <li key={index}>
+            {item}
+         </li>)
+   });
+
    function handleSubmit(e) {
+
       e.preventDefault();
       let factory = new SimpleCreator();
       try {
-         console.log(factory.createShape({ option: e.target }, shapeNames.square).description())
-         console.log(factory.createShape({ option: e.target }, shapeNames.triangle).description())
-         console.log(factory.createShape({ option: e.target }, shapeNames.circle).description())
-         console.log(factory.createShape({ option: e.target }, shapeNames.rectangle).description())
+         result.push(factory.createShape({ option: e.target }, shapeNames.square).description())
+         result.push(factory.createShape({ option: e.target }, shapeNames.rectangle).description())
+         result.push(factory.createShape({ option: e.target }, shapeNames.triangle).description())
+         result.push(factory.createShape({ option: e.target }, shapeNames.circle).description())
+         var mapResult = result.map((item, index) =>
+            <li key={index}>
+               {item}
+            </li>);
+         setResultItems({ jsx: mapResult })
 
       } catch (error) {
          console.error(error.message)
@@ -27,47 +42,58 @@ function Form() {
    { defaultValue: 4, label: "height", name: "rectangleHeight" }];
 
    return (
-      <form onSubmit={handleSubmit}>
-         <div className="row">
-            <div className="col-md-3 col-sm-6 col-xs-12">
-               <div className="card">
-                  <h3 className="card-header">square</h3>
-                  <div className="card-body">
-                     <ShapeForm param={squareDim} />
+      <div>
+         <form onSubmit={handleSubmit}>
+            <div className="row">
+               <div className="col-md-3 col-sm-6 col-xs-12">
+                  <div className="card">
+                     <h3 className="card-header">square</h3>
+                     <div className="card-body">
+                        <ShapeForm param={squareDim} />
+                     </div>
+                  </div>
+               </div>
+               <div className="col-md-3 col-sm-6 col-xs-12">
+                  <div className="card">
+                     <h3 className="card-header">triangle</h3>
+                     <div className="card-body">
+                        <ShapeForm param={triangleDim} />
+                     </div>
+                  </div>
+               </div>
+               <div className="col-md-3 col-sm-6 col-xs-12">
+                  <div className="card">
+                     <h3 className="card-header">circle</h3>
+                     <div className="card-body">
+                        <ShapeForm param={circleDim} />
+                     </div>
+                  </div>
+               </div>
+               <div className="col-md-3 col-sm-6 col-xs-12">
+                  <div className="card">
+                     <h3 className="card-header">rectangle</h3>
+                     <div className="card-body">
+                        <ShapeForm param={rectangleDim} />
+                     </div>
                   </div>
                </div>
             </div>
-            <div className="col-md-3 col-sm-6 col-xs-12">
-               <div className="card">
-                  <h3 className="card-header">triangle</h3>
-                  <div className="card-body">
-                     <ShapeForm param={triangleDim} />
-                  </div>
+            <div className="row justify-content-center mt-3">
+               <div className="col-6">
+                  <button type="submit" className="btn btn-success col-12">Submit</button>
                </div>
             </div>
-            <div className="col-md-3 col-sm-6 col-xs-12">
-               <div className="card">
-                  <h3 className="card-header">circle</h3>
-                  <div className="card-body">
-                     <ShapeForm param={circleDim} />
-                  </div>
-               </div>
-            </div>
-            <div className="col-md-3 col-sm-6 col-xs-12">
-               <div className="card">
-                  <h3 className="card-header">rectangle</h3>
-                  <div className="card-body">
-                     <ShapeForm param={rectangleDim} />
-                  </div>
-               </div>
+         </form>
+         <div className="card row mt-3">
+            <h1 className="card-header">Result</h1>
+            <div className="card-body">
+               <ul>
+                  {resultItems.jsx}
+               </ul>
             </div>
          </div>
-         <div className="row justify-content-center mt-3">
-            <div className="col-6">
-               <button type="submit" className="btn btn-success col-12">Submit</button>
-            </div>
-         </div>
-      </form>
+
+      </div>
    );
 }
 
